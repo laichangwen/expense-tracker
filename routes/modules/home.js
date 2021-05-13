4b81c4d2e346392b25dedb72427ea6cf41b18931
laichangwen <laichangwen21@gmail.com>
2021-05-14 01:01:23 +0800

    feat: add calculate total amount

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,12 +2,27 @@ const express = require("express")
 const router = express.Router()
 const Record = require("../../models/record")
 
+
 router.get("/", (req, res) => {
+
   Record.find()
     .lean()
-    .sort("-category")
-    .then(records => res.render("index", { records }))
-    .catch(error => console.error(error))
+    .then((records) => {
+      // let totalamount = {}
+      Record.aggregate([
+        {
+          $group: {
+            _id: null,
+            amount: { $sum: "$amount" },
+          }
+        }
+      ]).then((amount) => {
+        totalamount = amount[0]
+        res.render("index", { records, totalamount })
+      })
+
+    })
+
 })
 
 module.exports = router
